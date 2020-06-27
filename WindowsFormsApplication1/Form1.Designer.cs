2dd97f04aebb40ec2fd4974da80fcd77499c46e5
He1ler <danisamo@i.ua>
2020-06-27 11:52:13 +0300

    1

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -1334,7 +1334,7 @@
             this.textBox1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.textBox1.Location = new System.Drawing.Point(940, 435);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(78, 23);
+            this.textBox1.Size = new System.Drawing.Size(78, 27);
             this.textBox1.TabIndex = 82;
             this.textBox1.Text = "МАССА (г)";
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -1349,7 +1349,7 @@
             this.label6.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label6.Location = new System.Drawing.Point(902, 407);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(360, 18);
+            this.label6.Size = new System.Drawing.Size(450, 22);
             this.label6.TabIndex = 92;
             this.label6.Text = "РОЗРАХУНОК КІЛЬКОСТІ МОЛЕКУЛ, ЩО РОЗПАДЕТЬСЯ";
             this.label6.Visible = false;
@@ -1359,7 +1359,7 @@
             this.textBox3.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.textBox3.Location = new System.Drawing.Point(1099, 435);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(65, 23);
+            this.textBox3.Size = new System.Drawing.Size(65, 27);
             this.textBox3.TabIndex = 95;
             this.textBox3.Text = "ДНІ";
             this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -1375,7 +1375,7 @@
             this.textBox4.Location = new System.Drawing.Point(1169, 435);
             this.textBox4.Margin = new System.Windows.Forms.Padding(2);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(65, 23);
+            this.textBox4.Size = new System.Drawing.Size(65, 27);
             this.textBox4.TabIndex = 96;
             this.textBox4.Text = "ГОДИНИ";
             this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -1390,7 +1390,7 @@
             this.textBox5.Location = new System.Drawing.Point(1241, 435);
             this.textBox5.MaxLength = 4;
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(73, 23);
+            this.textBox5.Size = new System.Drawing.Size(73, 27);
             this.textBox5.TabIndex = 97;
             this.textBox5.Tag = "";
             this.textBox5.Text = "CЕКУНДИ";
@@ -1406,7 +1406,7 @@
             this.label8.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label8.Location = new System.Drawing.Point(1003, 483);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(70, 15);
+            this.label8.Size = new System.Drawing.Size(90, 20);
             this.label8.TabIndex = 102;
             this.label8.Text = "РЕЗУЛЬТАТ";
             this.label8.Visible = false;
@@ -1425,7 +1425,7 @@
             this.label9.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label9.Location = new System.Drawing.Point(1132, 627);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(56, 15);
+            this.label9.Size = new System.Drawing.Size(72, 20);
             this.label9.TabIndex = 104;
             this.label9.Text = "ЕЛЕМЕНТ";
             // 
@@ -1495,7 +1495,7 @@
             this.textBox2.Location = new System.Drawing.Point(1024, 435);
             this.textBox2.Name = "textBox2";
             this.textBox2.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.textBox2.Size = new System.Drawing.Size(69, 23);
+            this.textBox2.Size = new System.Drawing.Size(69, 27);
             this.textBox2.TabIndex = 94;
             this.textBox2.Text = "РОКИ";
             this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -1608,7 +1608,7 @@
             this.label1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label1.Location = new System.Drawing.Point(996, 627);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(49, 15);
+            this.label1.Size = new System.Drawing.Size(63, 20);
             this.label1.TabIndex = 119;
             this.label1.Text = "ІЗОТОП";
             // 
@@ -2096,7 +2096,7 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(5, 619);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.Size = new System.Drawing.Size(59, 17);
             this.label2.TabIndex = 1000;
             this.label2.Text = "Автори:";
             // 
@@ -2105,16 +2105,16 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(70, 618);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(96, 13);
+            this.label3.Size = new System.Drawing.Size(124, 17);
             this.label3.TabIndex = 1001;
-            this.label3.Text = "Денис Корольчук";
+            this.label3.Text = "Корольчук Денис";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(70, 636);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.Size = new System.Drawing.Size(98, 17);
             this.label4.TabIndex = 1002;
             this.label4.Text = "Новак Богдан";
             // 
@@ -2123,7 +2123,7 @@
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(5, 665);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(191, 13);
+            this.label5.Size = new System.Drawing.Size(252, 17);
             this.label5.TabIndex = 1003;
             this.label5.Text = "Alma mater: NU LP KI-14 2019-2020 ©";
             // 
@@ -2241,18 +2241,19 @@
             // 
             // menuStrip1
             // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.інформаціяToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1344, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(1344, 30);
             this.menuStrip1.TabIndex = 1020;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // інформаціяToolStripMenuItem
             // 
             this.інформаціяToolStripMenuItem.Name = "інформаціяToolStripMenuItem";
-            this.інформаціяToolStripMenuItem.Size = new System.Drawing.Size(83, 20);
+            this.інформаціяToolStripMenuItem.Size = new System.Drawing.Size(104, 26);
             this.інформаціяToolStripMenuItem.Text = "Інформація";
             this.інформаціяToolStripMenuItem.Click += new System.EventHandler(this.інформаціяToolStripMenuItem_Click);
             // 
